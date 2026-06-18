a8e70e4a82509b268cc052fadbc9a6645baf3c0d
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-06-18 21:29:42 +0530

    plus symbol

---
+++ PlusStar.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the value of n";
+    int n;
+    cin>>n;
+    int mid=n/2 +1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==mid || j==mid) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
